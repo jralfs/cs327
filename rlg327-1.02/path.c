7f35cfb2b6e4eeaa28cd71c044d2fb81f7f9280d
Annie Steenson <anniesteenso@gmail.com>
2016-02-12 21:40:10 -0600

    created path.c

---
+++ rlg327-1.02/path.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "binheap.h"
+
+void dijkstra_nontunneling(dungeon_t *d){
+	binheap_t *h;
+	binheap_init(h, )
+}
+
+void compare(const binheap_node *key, const binheap_node *with){
+
+}
