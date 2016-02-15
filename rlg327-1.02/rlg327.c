557132e50d02bc788ce2db97f3eb7fa04e6198da
Annie Steenson <anniesteenso@gmail.com>
2016-02-15 15:27:06 -0600

    changes

--- rlg327-1.02/rlg327.c
+++ rlg327-1.02/rlg327.c
@@ -29,12 +29,6 @@ typedef struct corridor_path {
   int32_t cost;
 } corridor_path_t;
 
-typedef enum dim {
-  dim_x,
-  dim_y,
-  num_dims
-} dim_t;
-
 typedef int16_t pair_t[num_dims];
 
 #define DUNGEON_X              80
@@ -55,38 +49,6 @@ typedef int16_t pair_t[num_dims];
 #define hardnesspair(pair) (d->hardness[pair[dim_y]][pair[dim_x]])
 #define hardnessxy(x, y) (d->hardness[y][x])
 
-typedef enum __attribute__ ((__packed__)) terrain_type {
-  ter_debug,
-  ter_wall,
-  ter_wall_immutable,
-  ter_floor,
-  ter_floor_room,
-  ter_floor_hall,
-} terrain_type_t;
-
-typedef struct room {
-  pair_t position;
-  pair_t size;
-  uint32_t connected;
-} room_t;
-
-typedef struct dungeon {
-  uint32_t num_rooms;
-  room_t *rooms;
-  terrain_type_t map[DUNGEON_Y][DUNGEON_X];
-  /* Since hardness is usually not used, it would be expensive to pull it *
-   * into cache every time we need a map cell, so we store it in a        *
-   * parallel array, rather than using a structure to represent the       *
-   * cells.  We may want a cell structure later, but from a performanace  *
-   * perspective, it would be a bad idea to ever have the map be part of  *
-   * that structure.  Pathfinding will require efficient use of the map,  *
-   * and pulling in unnecessary data with each map cell would add a lot   *
-   * of overhead to the memory system.                                    */
-  uint8_t hardness[DUNGEON_Y][DUNGEON_X];
-  char nonTunnel[DUNGEON_Y][DUNGEON_X];
-  char tunnel[DUNGEON_Y][DUNGEON_X];
-  pair_t PC; 
-} dungeon_t;
 
 static int32_t corridor_path_cmp(const void *key, const void *with) {
   return ((corridor_path_t *) key)->cost - ((corridor_path_t *) with)->cost;
