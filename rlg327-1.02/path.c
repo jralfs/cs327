677381edb3d42fd75db2fcf2b4b19b2a6fa69ab5
lilannie <lilannie@iastate.edu>
2016-02-15 22:13:56 -0600

    more changes

--- rlg327-1.02/path.c
+++ rlg327-1.02/path.c
@@ -14,8 +14,8 @@ typedef struct vertex {
 
 
 int32_t compare_data(const void *key, const void *with){
-  	vertex vKey = &key;
-  	vertex wKey = &with;
+  	vertex_t vKey = &key;
+  	vertex_t wKey = &with;
   	return vKey.distance - wKey.distance;
  }
 
